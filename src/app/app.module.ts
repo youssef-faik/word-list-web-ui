7ed2ed29d3cbb4553a196d07f621749c4a9fb353
Youssef Faik <faik.yusef@gmail.com>
2023-02-26 22:10:16 +0100

    add app-routing module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,18 +5,12 @@ import { AppComponent } from './app.component';
 import { WordsModule } from './words/words.module';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 import { ConfirmDialogComponent } from './dialogs/confirm-dialog/confirm-dialog.component';
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    PageNotFoundComponent,
-    ConfirmDialogComponent
-  ],
-  imports: [
-    BrowserModule,
-    WordsModule
-  ],
+  declarations: [AppComponent, PageNotFoundComponent, ConfirmDialogComponent],
+  imports: [BrowserModule, WordsModule, AppRoutingModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
